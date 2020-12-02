025d3873d66439678c53e9b5a96604678816b07f
Jaden <moorejaden5389@gmail.com>
2020-12-01 19:16:52 -0800

    format header files

--- Extra Credit 1/SLL.h
+++ Extra Credit 1/SLL.h
@@ -1,31 +1,45 @@
+/*
+	Huang, Yitian
+	Moore, Jaden
+	
+	December 1, 2020
+
+	CS A250
+	Extra Credit 1
+*/
+
 #ifndef SLL_H
 #define SLL_H
 
 #include "Node.h"
-#include "Node.cpp"
 #include <iostream>
 
 template<typename T>
 class SLL
 {
-template<typename T>
-friend std::ostream& operator<<(std::ostream& out, const SLL<T>& rhs);
+	template<typename G>
+	friend std::ostream& operator<<( std::ostream& out, const SLL<G>& otherList );
+
+public:
+	SLL() : m_first( nullptr ), m_count( 0 ) {}
+
+	SLL<T>( const SLL<T>& copy );
+
+	SLL<T>& operator=( const SLL<T>& otherList );
+
+	void push( const T& element );
+
+	T pop();
+
+	bool find( const T& element ) const;
+
+	void clear();
+
+	~SLL() { clear(); }
+
 private:
 	Node<T>* m_first;
 	int m_count;
-
-public:
-	SLL() : m_first(nullptr), m_count(0) {}
-
-	SLL<T>(const SLL<T>& other);
-	SLL<T>& operator=(const SLL<T>& rhs);
-
-	void push(const T& element);
-	T pop();
-	bool find(const T& element) const;
-	void clear();
-	 
-	~SLL() { clear(); }
 };
 
 #endif
